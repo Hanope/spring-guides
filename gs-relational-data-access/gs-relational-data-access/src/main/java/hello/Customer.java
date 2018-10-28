0527e5bd9f0d76179f62c88a6a49c0749386a613
Hanope <heemanghan@gmail.com>
2018-10-28 16:02:07 +0900

    http://spring.io/guides/gs/relational-data-access/ 번역 및 예제 파일

---
+++ gs-relational-data-access/gs-relational-data-access/src/main/java/hello/Customer.java
@@ -0,0 +1,44 @@
+package hello;
+
+public class Customer {
+    private long id;
+    private String firstName, lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
+
