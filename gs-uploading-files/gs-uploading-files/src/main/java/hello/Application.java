3efc8dcea85e0633a966aabcbb8a031215fb0290
Hanope <heemanghan@gmail.com>
2018-11-04 23:58:18 +0900

    http://spring.io/guides/gs/uploading-files/ 번역 및 예제 파일

---
+++ gs-uploading-files/gs-uploading-files/src/main/java/hello/Application.java
@@ -0,0 +1,27 @@
+package hello;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import hello.storage.StorageProperties;
+import hello.storage.StorageService;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
